79747932778af850fb71ceeaa03fbcb64cda5d3e
beit-hatalmud <6787012@gmail.com>
2026-08-11 17:57:29 +0300

    Connect to Supabase (beit-hatalmud project) + protect local secrets

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-window.CV3={INSTANCE:'beit-hatalmud',INSTANCE_NAME:'מכינה בית התלמוד',SUPABASE_URL:'',SUPABASE_ANON_KEY:''};
+window.CV3={INSTANCE:'beit-hatalmud',INSTANCE_NAME:'מכינה בית התלמוד',SUPABASE_URL:'https://jpcepdbhouuwpjdidqfo.supabase.co',SUPABASE_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwY2VwZGJob3V1d3BqZGlkcWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY0NTY1NDIsImV4cCI6MjEwMjAzMjU0Mn0.SR1cwgyI6eDv5aXqPkL4Nu0UINuvaWIh4jtElO44LS0'};
 window.CV3.DEMO=!window.CV3.SUPABASE_URL||!window.CV3.SUPABASE_ANON_KEY;
